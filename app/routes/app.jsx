ad21306265fc2abc1ff7867dbe859c989dcfdf0a
Techexplified <tech@explified.com>
2026-07-13 22:16:59 +0530

    fix

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -2,7 +2,7 @@ import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+
 
 export const loader = async ({ request }) => {
   const {session} = await authenticate.admin(request);
