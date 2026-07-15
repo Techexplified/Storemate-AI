6d2efef57ae7dd9a22b3824294950a901b6dd5bf
Techexplified <tech@explified.com>
2026-07-15 20:50:38 +0530

    added compilance webhooks

---
+++ app/routes/webhooks.jsx
@@ -0,0 +1,14 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  const { topic } = await authenticate.webhook(request);
+  
+  switch (topic) {
+    case "CUSTOMERS_DATA_REQUEST":
+    case "CUSTOMERS_REDACT":
+    case "SHOP_REDACT":
+      return new Response(null, { status: 200 });
+    default:
+      return new Response("Unhandled topic", { status: 404 });
+  }
+};
